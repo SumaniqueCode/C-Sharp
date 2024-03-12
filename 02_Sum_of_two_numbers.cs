1cad398b4efc48dfee58df421e361da301ebb47f
Limitless Playing <limitlessplaying@gmail.com>
2024-03-12 15:55:50 +0545

    data conversion and scanner in C#

---
+++ 02_Sum_of_two_numbers.cs
@@ -0,0 +1,14 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int a, b, sum;
+        Console.Write("Enter the first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+        sum = a+b;
+        Console.WriteLine("The sum is {0}",sum);
+    }
+}
