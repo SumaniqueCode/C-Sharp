a87dd34ffa16f504793bf6eaaabf24ce8bc83e86
Limitless Playing <limitlessplaying@gmail.com>
2024-05-06 12:07:39 +0545

    Multi cast deligate

---
+++ delegate.cs
@@ -0,0 +1,32 @@
+//Example of deligate for adding two numbers
+//It is also example of multicast deligate
+
+using System;
+
+class DelegateTest
+{
+    public delegate int DeligateNum(int x, int y);
+
+    public static int AddNum(int x, int y)
+    {
+        int result = x + y;
+        return result;
+    }
+
+    public static int MulNum(int x, int y)
+    {
+        int result = x * y;
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        DeligateNum sum = new DeligateNum(AddNum);
+        Console.WriteLine(sum(5, 2));
+        DeligateNum mul = new DeligateNum(MulNum);
+        Console.WriteLine(mul(10, 12));
+
+
+    }
+}
+
